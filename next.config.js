49c67ef73a11b537f85838b1ac88170204bb8cbe
Chris Vinciguerra <christophervin1@gmail.com>
2024-05-02 01:04:56 -0400

    test

--- next.config.js
+++ next.config.js
@@ -1,8 +1,28 @@
 /** @type {import('next').NextConfig} */
-
 const nextConfig = {
-  output: 'export',
-  reactStrictMode: false,
-}
+  /**
+   * Enable static exports for the App Router.
+   *
+   * @see https://nextjs.org/docs/app/building-your-application/deploying/static-exports
+   */
+  output: "export",
 
-module.exports = nextConfig
+  /**
+   * Set base path. This is the slug of your GitHub repository.
+   *
+   * @see https://nextjs.org/docs/app/api-reference/next-config-js/basePath
+   */
+  basePath: "/ChrisVinciguerra.github.io",
+
+  /**
+   * Disable server-based image optimization. Next.js does not support
+   * dynamic features with static exports.
+   *
+   * @see https://nextjs.org/docs/app/api-reference/components/image#unoptimized
+   */
+  images: {
+    unoptimized: true,
+  },
+};
+
+export default nextConfig;
