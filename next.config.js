2a51feabd87b687731676704df6080e56151d6a1
Chris Vinciguerra <christophervin1@gmail.com>
2024-05-02 01:20:41 -0400

    attempt again

--- next.config.js
+++ next.config.js
@@ -7,12 +7,6 @@ const nextConfig = {
    */
   output: "export",
 
-  /**
-   * Set base path. This is the slug of your GitHub repository.
-   *
-   * @see https://nextjs.org/docs/app/api-reference/next-config-js/basePath
-   */
-  basePath: "/ChrisVinciguerra.github.io",
 
   /**
    * Disable server-based image optimization. Next.js does not support
